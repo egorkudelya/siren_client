f130c7ca6501fef164f511fbac7f34cac36cf940
egorkudelya <egorkudelya@users.noreply.github.com>
2023-07-18 15:16:59 +0300

    Initial Commit

---
+++ SirenClient/SirenClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SirenClientApp.swift
+//  SirenClient
+//
+//  Created by Egor Kudelia on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SirenClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
